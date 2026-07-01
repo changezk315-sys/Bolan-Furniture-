a0ec8d8ab7cae894949ce9d0b3223a008fb942ab
changezk315-sys <changezk315@gmail.com>
2026-07-01 17:02:12 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,98 @@
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("Bolan Furniture Website Loaded!");
+// Smooth Active Navigation
 
-    const contactBtn = document.querySelector(".hero a");
+const links = document.querySelectorAll("nav a");
 
-    contactBtn.addEventListener("click", function (e) {
-        e.preventDefault();
+links.forEach(link => {
 
-        // Yahan apna WhatsApp number likhna
-        window.open("https://wa.me/923001234567", "_blank");
-    });
+link.addEventListener("click", function(e){
+
+e.preventDefault();
+
+const target = document.querySelector(this.getAttribute("href"));
+
+target.scrollIntoView({
+behavior:"smooth"
 });
+
+});
+
+});
+
+// Header Shadow
+
+window.addEventListener("scroll", ()=>{
+
+const header = document.querySelector("header");
+
+if(window.scrollY > 50){
+
+header.style.boxShadow = "0 10px 20px rgba(0,0,0,.25)";
+header.style.background = "#3b2417";
+
+}else{
+
+header.style.boxShadow = "none";
+header.style.background = "#4e342e";
+
+}
+
+});
+
+// Fade In Animation
+
+const cards = document.querySelectorAll(".card,.feature-box,.about,.contact");
+
+const observer = new IntersectionObserver(entries=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.style.opacity="1";
+entry.target.style.transform="translateY(0)";
+
+}
+
+});
+
+});
+
+cards.forEach(card=>{
+
+card.style.opacity="0";
+card.style.transform="translateY(60px)";
+card.style.transition="1s";
+
+observer.observe(card);
+
+});
+
+// Button Ripple Effect
+
+document.querySelectorAll(".btn").forEach(btn=>{
+
+btn.addEventListener("mouseenter",()=>{
+
+btn.style.transform="scale(1.05)";
+
+});
+
+btn.addEventListener("mouseleave",()=>{
+
+btn.style.transform="scale(1)";
+
+});
+
+});
+
+// Gallery Image Hover
+
+document.querySelectorAll(".gallery img").forEach(img=>{
+
+img.addEventListener("mouseenter",()=>{
+
+img.style.filter="brightness(110%)";
+
+});
+
+img.addEventListener("
